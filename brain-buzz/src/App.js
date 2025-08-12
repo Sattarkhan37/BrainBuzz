fb8ffec6160b1633d683f79bd4767492cb470029
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-12 20:26:32 +0530

    feat: add quiz landing page with header, start screen UI, and initial state setup

--- brain-buzz/src/App.js
+++ brain-buzz/src/App.js
@@ -1,13 +1,53 @@
 import Main from "./Main";
 import Header from "./Header";
+import { useEffect, useReducer } from "react";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
+import StartScreen from "./StartScreen.js";
+import Question from "./Question.js";
+const initialState = {
+  questions: [],
+  //loading,'error','active','finisher'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+      case 'start':
+        return{...state,status:'active'}
+
+    default:
+      throw new Error("Action is Unknow");
+  }
+}
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQUestion = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div>
       <div className="app">
         <Header />
-        <Main className="main">
-          <p>1/15</p>
-          <p>Question?</p>
+        <Main>
+          {status === "loading" && <Loader />}
+          {status === "error" && <Error />}
+          {status === "ready" && <StartScreen numQUestion={numQUestion} dispatch={dispatch}/>}
+          {status ==="active"&&<Question/>}
         </Main>
       </div>
     </div>
---
+++ brain-buzz/src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen({numQUestion,dispatch}) {
+  return (
+    <div className="start">
+      <h2>Welocme to React Quiz</h2>
+      <h3>{numQUestion} Question to Test your React mastery</h3>
+      <button className="btn btn-ui" >lets Start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
